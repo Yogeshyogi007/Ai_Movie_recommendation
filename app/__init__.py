1bd8c732297620c918dfe435bee185f6f7b48c90
Yogeshyogi007 <129869608+Yogeshyogi007@users.noreply.github.com>
2025-05-09 22:30:16 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -16,12 +16,16 @@ def create_app():
     
     # Configure Flask app
     app.config['SECRET_KEY'] = environ.get('SECRET_KEY', 'dev')
-    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get(
-        'DATABASE_URL', '').replace('postgres://', 'postgresql://')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL').replace('postgres://', 'postgresql://')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Initialize extensions
     db.init_app(app)
+    with app.app_context():
+        try:
+            db.create_all()
+        except Exception as e:
+            print(f"Error creating tables: {e}")
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
 
