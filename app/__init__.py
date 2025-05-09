870f9820b60ca22c42e4c42a20cc6755425f4860
Yogeshyogi007 <129869608+Yogeshyogi007@users.noreply.github.com>
2025-05-09 22:33:20 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from os import environ
+import os
 from dotenv import load_dotenv
 
 # Load environment variables
